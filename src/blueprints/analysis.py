c1a0cae58ba8560c7ce1bbcb93b31b52bb280586
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-01-30 16:44:11 +0200

    bug in accessToken

--- src/blueprints/analysis.py
+++ src/blueprints/analysis.py
@@ -1,14 +1,24 @@
 from flask import Blueprint, request, Response
+from src.simulation import run
+from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt
 import json
 import multiprocessing as mp
-from src.simulation import run
 
 
 analysis = Blueprint("analysis", __name__)
 
 
 @analysis.route("/execute", methods=["POST"])
+@jwt_required()
 def execute():
+	# Get the user id from the JWT
+	current_user = get_jwt_identity()
+	claims = get_jwt()
+
+	if not claims["isEnabled"]: return Response(response=json.dumps({"error": "User is not authorized to access this page"}), status=401)
+
+	print(f"Current user: {current_user}, claims: {claims}")
+
 	# Get the data from the request
 	data = request.get_json()
 	user_id, num1, num2 = data["user_id"], data["num1"], data["num2"]
