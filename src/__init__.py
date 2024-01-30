8aa97df5e595860711d9cd1e6fcad904f8efe308
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2024-01-30 13:04:44 +0200

    jwt and google auth

--- src/__init__.py
+++ src/__init__.py
@@ -1,24 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from flask_swagger_ui import get_swaggerui_blueprint
 from mongoengine import connect
 from src.config import *
 
 from src.blueprints.analysis import analysis
-
-
-SWAGGER_URL = "/docs"
-API_URL = "/static/swagger.json"
-
-swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        "app_name": "Digital Twin of Attica API",
-    },
-)
+from src.blueprints.auth import auth
 
 app = Flask(__name__)
+jwt = JWTManager(app)
+
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
 
 connect(alias=MONGO_DBNAME, db=MONGO_DBNAME, host=MONGO_HOST, port=MONGO_PORT)
 
@@ -29,6 +22,17 @@ cors = CORS(
     },
 )
 
-app.register_blueprint(analysis, url_prefix="/analysis")
+SWAGGER_URL = "/docs"
+API_URL = "/static/swagger.json"
 
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        "app_name": "Eydap tool",
+    },
+)
+
+app.register_blueprint(analysis, url_prefix="/analysis")
+app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(swaggerui_blueprint)
--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 MONGO_DBNAME = 'eydap'
+GOOGLE_AUDIENCE = "503881624899-7335rgpn7abu249ocar66cda3q5i97nn.apps.googleusercontent.com"
+JWT_SECRET_KEY = "rQY3B8WQvrqMFVhqXKq9VhGUuwYCdMhQ"
